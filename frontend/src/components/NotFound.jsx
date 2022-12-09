e9ad4ca5c3593f799b207d460fec3f82b445e412
D'Arcy <walterluke1612@gmail.com>
2022-12-09 19:02:57 +0630

    ss

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div className='h-screen w-screen text-primary font-head flex items-center justify-center text-3xl'>
+    <div className='h-screen flex-col w-screen gap-10 text-primary font-head flex items-center justify-center text-3xl'>
         Something Went Wrong !!
+        <Link className='underline text-base text-white font-body' to = '/'>
+            Back to Login Page..
+        </Link>
     </div>
   )
 }
